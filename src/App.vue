562661c0ce09e96ac25c9327e7048ba5c155a924
Bonifacio <revanjz6@gmail.com>
2019-02-04 12:51:25 -0600

    agragando de talles al nav

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
   <v-app>
-    <v-toolbar color="primary" app>
+    <v-toolbar  color="primary" class="text-md-center" app>
+      <section id="logo">
+        <v-toolbar-side-icon class="hidden-md-and-up"></v-toolbar-side-icon>
+          <a href="#">
+            <v-btn icon>
+              <v-img :src="logo" height="32px" width="32px"></v-img>
+            </v-btn>
+            </a>
+      </section>
+        <v-spacer></v-spacer>
+      <section id="titulo">
+        <section class="hidden-sm-and-down" >
+          <v-toolbar-title >
+           <span>Ingenieria en Tecnologias de la Informacion y Comunicaciones</span>
+          </v-toolbar-title>
+        </section>
+        <section class="hidden-md-and-up">
+          <v-toolbar-title>
+            <span>ITIC</span>
+          </v-toolbar-title>
+        </section>
+      </section>
       
-      <v-toolbar-title  class="headline text-uppercase">
-        <a href="#">
-          <v-btn icon>
-           <v-img :src="logo" height="32px" width="32px"></v-img>
-          </v-btn>
-        </a>
-      </v-toolbar-title>
     </v-toolbar>
 
     <v-content>
