9599d01de33bbc528162c784fd2762d285d98de4
Bonifacio <revanjz6@gmail.com>
2019-02-04 18:57:31 -0600

    Componente Navigation

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -34,21 +34,6 @@ import logo from '../assets/Calca_TIC.png'
         data() {
             return{
             logo,
-            drawer: true,
-      items: [
-        { icon: 'trending_up', text: 'Most Popular' },
-        { icon: 'subscriptions', text: 'Subscriptions' },
-        { icon: 'history', text: 'History' },
-        { icon: 'featured_play_list', text: 'Playlists' },
-        { icon: 'watch_later', text: 'Watch Later' }
-      ],
-      items2: [
-        { picture: 28, text: 'Joseph' },
-        { picture: 38, text: 'Apple' },
-        { picture: 48, text: 'Xbox Ahoy' },
-        { picture: 58, text: 'Nokia' },
-        { picture: 78, text: 'MKBHD' }
-      ]
             }
         }
     }
---
+++ src/components/Navigation.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-navigation-drawer
+     fixed
+      clipped
+      v-model="drawer"
+      mobile-break-point="960"
+      app
+    >
+    </v-navigation-drawer>
+    
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            drawer: true
+    
+        }
+    }
+}
+</script>
